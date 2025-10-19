f33eb35305355951d790cb37702b089d4a083bd6
junaid-sarwar <junaidsarwariqbal@gmail.com>
2025-10-19 22:56:29 +0500

    fixed cors error

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ Allow requests from your React frontend
 app.use(
   cors({
-    origin: "https://smit-final-hackathon-jnd.vercel.app/", // React app origin
+    origin: "https://smit-final-hackathon-jnd.vercel.app", // React app origin
     credentials: true,
   })
 );
